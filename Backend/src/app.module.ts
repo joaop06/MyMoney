e6750e625fdb9105c0717970323dd39c43b3d5b9
João Pedro <joaoofficialpedro@gmail.com>
2024-09-25 00:41:16 -0300

    Feat: Added config db

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,29 +1,27 @@
+require('dotenv').config()
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
-import { UserEntity } from './users/user.entity';
 import { UsersModule } from './users/users.module';
-
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
-    TypeOrmModule.forRoot({
-      port: 3306,
-      type: 'mysql',
-      synchronize: true,
-      host: '189.90.130.58',
-      password: 'Joao55347@',
-      entities: [UserEntity],
-      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      database: 'joaopedro_mymoney',
-      username: 'joaopedro_mymoney',
-    }),
+    AuthModule,
     UsersModule,
-    AuthModule
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      port: parseInt(process.env.DB_PORT),
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
   ],
   controllers: [AppController],
   providers: [AppService],

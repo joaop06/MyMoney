84db3f1248f23104d0af93128d6da12805962d4c
João Pedro <joaoofficialpedro@gmail.com>
2024-11-09 10:14:43 -0300

    Fix: Added library bcryptjs

--- Backend/src/auth/auth.service.ts
+++ Backend/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
 import { LoginDto } from './dtos/login.dto';
--- Backend/src/users/users.service.ts
+++ Backend/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { UserEntity } from './user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
